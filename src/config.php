324cbfa7e6e0c009a5e9ca231e440a7754e7b000
Tafelglotzer <nico.rickenbach@gmail.com>
2017-06-27 14:35:51 +0200

    Updated config

--- src/config.php
+++ src/config.php
@@ -4,7 +4,7 @@ $config = [
     /**
      * Where the logs are stored
      */
-    'logfile' => 'logs/updater.log',
+    'logfile' => 'src/logs/updater.log',
 
     /**
      * Used for logging the current date and time
@@ -14,12 +14,12 @@ $config = [
     /**
      * Path to your album1584
      */
-    'album1584' => 'swfs/c_images/album1584',
+    'album1584' => 'C:/inetpub/wwwroot/swfs/c_images/album1584',
 
     /**
      * Path to your external_flash_texts
      */
-    'flash_texts' => 'swfs/gamedata/external_flash_texts.txt',
+    'flash_texts' => 'C:/inetpub/wwwroot/swfs/gamedata/external_flash_texts.txt',
 
     /**
      * Badges are downloaded from this locales
